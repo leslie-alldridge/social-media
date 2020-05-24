b9c805ba3bfc30b12f558908d7dade34ed3e023a
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-24 18:00:03 +1200

    getScreams from datastore

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
