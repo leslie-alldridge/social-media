e47d3d9dfafe1bc660e78b0a86402d848a3ca96e
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-27 21:34:23 +1200

    createscream endpoint

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -23,3 +23,27 @@ exports.getScreams = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(400).json({ error: `Method not allowed` });
+  }
+
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `Document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: `Something went wrong` });
+      console.error(err);
+    });
+});
