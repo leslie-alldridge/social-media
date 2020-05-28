dae4e1c4ef9dbf952f89fe09e897ede9c363a63f
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-28 18:54:51 +1200

    validation added

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -48,6 +48,17 @@ app.post('/screams', (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
+  else return false;
+};
+
 // Sign up route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -56,6 +67,23 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = 'Passwords must match';
+
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
