c8890218cf5ddfa6cc171b39c3223f20bcde2cef
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-31 11:50:42 +1200

    middleware auth

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -41,6 +41,25 @@ const FBAuth = (req, res, next) => {
     console.error('No token found');
     return res.status(403).json({ error: 'Unauthorized' });
   }
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      return db
+        .collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error('Error while verifying token', err);
+      return res.status(403).json(err);
+    });
 };
 
 app.post('/scream', FBAuth, (req, res) => {
